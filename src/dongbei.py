9b7a12425f0771780e12618b8747645176bac1e5
Zhanyong Wan <shiqian@gmail.com>
2020-01-20 00:24:50 -0800

    Supports basic print and assignment.

--- src/dongbei.py
+++ src/dongbei.py
@@ -1,27 +1,273 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import io
 import re
+import sys
+
+KW_BECOME = u'装'
+KW_CLOSE_QUOTE = u'”'
+KW_COLON = u'：'
+KW_IS_VAR = u'是活雷锋'
+KW_OPEN_QUOTE = u'“'
+KW_PERIOD = u'。'
+KW_SAY = u'唠'
+
+KEYWORDS = (
+    KW_BECOME,
+    KW_CLOSE_QUOTE,
+    KW_COLON,
+    KW_IS_VAR,
+    KW_OPEN_QUOTE,
+    KW_PERIOD,
+    KW_SAY,
+    )
+
+# Types of tokens.
+TK_KEYWORD = 'KEYWORD'
+TK_IDENTIFIER = 'IDENTIFIER'
+TK_STRING_LITERAL = 'STRING'
+TK_INTEGER_LITERAL = 'INTEGER'
+TK_CHAR = 'CHAR'
+
+# Parser states.
+ST_SAY = 'SAY'
+ST_SAY_COLON = 'SAY_COLON'
+ST_IDENTIFIER = 'IDENTIFIER'
+
+# Statements.
+STMT_SAY = 'SAY'
+STMT_VAR_DECL = 'VAR_DECL'
+STMT_ASSIGN = 'ASSIGN'
+
+class Token:
+  def __init__(self, kind, value):
+    self.kind = kind
+    self.value = value
+
+  def __str__(self):
+    return self.__unicode()
+
+  def __unicode__(self):
+    return u'%s <%s>' % (self.kind, self.value)
+
+  def __repr__(self):
+    return self.__unicode__().encode('utf-8')
+
+  def __eq__(self, other):
+    return other is not None and self.kind == other.kind and self.value == other.value
+
+  def __ne__(self, other):
+    return not (self == other)
+
+def TokenizeStringLiteralAndRest(code):
+  close_quote_pos = code.find(KW_CLOSE_QUOTE)
+  if close_quote_pos < 0:
+    yield Token(TK_STRING_LITERAL, code)
+    return
+
+  yield Token(TK_STRING_LITERAL, code[:close_quote_pos])
+  last_token = Token(TK_KEYWORD, KW_CLOSE_QUOTE)
+  yield last_token
+  for tk in BasicTokenize(code[close_quote_pos + len(KW_CLOSE_QUOTE):], last_token):
+    yield tk
+
+
+def BasicTokenize(code, last_token=None):
+  # Skip spaces at the beginning.
+  while True:
+    old_len = len(code)
+    code = code.lstrip()
+    if code.startswith('#'):  # comment
+      code = re.sub(r'^.*', '', code)  # Ignore the comment line.
+    if len(code) == old_len:
+      break
+    
+  if not code:
+    return
+
+  # Try to parse a keyword at the beginning of the code.
+  for keyword in KEYWORDS:
+    if code.startswith(keyword):
+      last_token = Token(TK_KEYWORD, keyword)
+      yield last_token
+      for tk in BasicTokenize(code[len(keyword):].lstrip(), last_token):
+        yield tk
+      return
+
+  # The code doesn't start with a keyword.
+  # Try to parse a string literal.
+  if last_token == Token(TK_KEYWORD, KW_OPEN_QUOTE):
+    for tk in TokenizeStringLiteralAndRest(code):
+      yield tk
+    return
+
+  last_token = Token(TK_CHAR, code[0])
+  yield last_token
+  for tk in BasicTokenize(code[1:], last_token):
+    yield tk
+  
+
+def ParseInteger(str):
+  m = re.match(r'^([0-9]+)(.*)', str)
+  if m:
+    return (int(m.group(1)), m.group(2))
+  return (None, str)
+
+    
+def ParseChars(chars):
+  integer, rest = ParseInteger(chars)
+  if integer is not None:
+    yield Token(TK_INTEGER_LITERAL, integer)
+  if rest:
+    yield Token(TK_IDENTIFIER, rest)
+
+def Tokenize(code):
+  last_token = Token(None, None)
+  chars = ''
+  for token in BasicTokenize(code):
+    last_last_token = last_token
+    last_token = token
+    if token.kind == TK_CHAR:
+      if last_last_token.kind == TK_CHAR:
+        chars += token.value
+        continue
+      else:
+        chars = token.value
+        continue
+    else:
+      if last_last_token.kind == TK_CHAR:
+        # A sequence of consecutive TK_CHARs ended.
+        for tk in ParseChars(chars):
+          yield tk
+      yield token
+      chars = ''
+  for tk in ParseChars(chars):
+    yield tk
+    
+vars = {}  # Maps Chinese identifier to generated identifier.
+def GetPythonVarName(var):
+  if var in vars:
+    return vars[var]
+
+  generated_var = '_db_var%d' % (len(vars),)
+  vars[var] = generated_var
+  return generated_var
+
+def TryConsumeTokenType(tk_type, tokens):
+  if not tokens:
+    return (None, tokens)
+  if tokens[0].kind == tk_type:
+    return (tokens[0], tokens[1:])
+  return (None, tokens)
+
+def ConsumeTokenType(tk_type, tokens):
+  tk, tokens = TryConsumeTokenType(tk_type, tokens)
+  if tk is None:
+    sys.exit(u'期望 %s，实际不是。' % (tk_type,))
+  return tk, tokens
+    
+def TryConsumeToken(token, tokens):
+  if not tokens:
+    return (None, tokens)
+  if token != tokens[0]:
+    return (None, tokens)
+  return (token, tokens[1:])
+
+def ConsumeToken(token, tokens):
+  if not tokens:
+    sys.exit(u'语句结束太早。')
+  if token != tokens[0]:
+    sys.exit(u'期望符号 %s，实际却是 %s。' %
+             (token, tokens[0]))
+  return token, tokens[1:]
+
+def ParseExpression(tokens):
+  integer, tokens = TryConsumeTokenType(TK_INTEGER_LITERAL, tokens)
+  if integer is not None:
+    return integer, tokens
+  var, tokens = TryConsumeTokenType(TK_IDENTIFIER, tokens)
+  if var is not None:
+    return var, tokens
+  _, tokens = ConsumeToken(Token(TK_KEYWORD, KW_OPEN_QUOTE), tokens)
+  str_token, tokens = ConsumeTokenType(TK_STRING_LITERAL, tokens)
+  _, tokens = ConsumeToken(Token(TK_KEYWORD, KW_CLOSE_QUOTE), tokens)
+  return str_token, tokens
+  
+  
+def TranslateToAst(tokens, statements):
+  if not tokens:
+    return
+
+  # print('1 %s' % (tokens,))
+  say, tokens = TryConsumeToken(Token(TK_KEYWORD, KW_SAY), tokens)
+  if say:
+    colon, tokens = ConsumeToken(Token(TK_KEYWORD, KW_COLON), tokens)
+    # print('%s' % (tokens,))
+    expr, tokens = ParseExpression(tokens)
+    _, tokens = ConsumeToken(Token(TK_KEYWORD, KW_PERIOD), tokens)
+    statements.append((STMT_SAY, expr))
+  else:
+    # print('2 %s' % (tokens,))
+    id, tokens = TryConsumeTokenType(TK_IDENTIFIER, tokens)
+    if not id:
+      sys.exit(u'语句必须以“唠”或者标识符开始。')
+    # print('3 %s' % (tokens,))
+    python_id = GetPythonVarName(id.value)
+    is_var, tokens = TryConsumeToken(Token(TK_KEYWORD, KW_IS_VAR), tokens)
+    # print('4 %s' % (tokens,))
+    if is_var:
+      _, tokens = ConsumeToken(Token(TK_KEYWORD, KW_PERIOD), tokens)
+      # print('5 %s' % (tokens,))
+      statements.append((STMT_VAR_DECL, python_id))
+    else:
+      become, tokens = TryConsumeToken(Token(TK_KEYWORD, KW_BECOME), tokens)
+      if become:
+        expr, tokens = ParseExpression(tokens)
+        _, tokens = ConsumeToken(Token(TK_KEYWORD, KW_PERIOD), tokens)
+        statements.append((STMT_ASSIGN, (python_id, expr)))
+      else:
+        sys.exit(u'名字过后应该是“是活雷锋”或者“装”。')
+
+  TranslateToAst(tokens, statements)  
+
+def TranslateExpression(expr):
+  if expr.kind == TK_INTEGER_LITERAL:
+    return '%s' % (expr.value,)
+  if expr.kind == TK_IDENTIFIER:
+    return GetPythonVarName(expr.value)
+  if expr.kind == TK_STRING_LITERAL:
+    return 'u"%s"' % (expr.value,)
+  sys.exit(u'我不懂 %s 表达式。' % (expr,))
+
+def TranslateStatement(stmt):
+  if stmt[0] == STMT_VAR_DECL:
+    return '%s = None' % (stmt[1],)
+  if stmt[0] == STMT_ASSIGN:
+    var, expr = stmt[1]
+    return '%s = %s' % (var, TranslateExpression(expr))
+  if stmt[0] == STMT_SAY:
+    expr = stmt[1]
+    return '_db_output += "%%s\\n" %% (%s,)' % (TranslateExpression(expr),)
+  sys.exit(u'我不懂 %s 语句。' % (stmt[0]))
+  
+def Translate(tokens):
+  statements = []
+  TranslateToAst(tokens, statements)
+  py_code = ['_db_output = ""']
+  for s in statements:
+    py_code.append(TranslateStatement(s))
+  return '\n'.join(py_code)
 
 def Run(code):
-  lines = code.split()
-  output = ''
-  value = None
-  for line in lines:
-    if re.match(u'.*是活雷锋。$', line):
-      value = None
-      continue
+  tokens = list(Tokenize(code))
+  py_code = Translate(tokens)
+  # print '%s' % (py_code,)
+  exec(py_code)
+  print('%s' % (_db_output,))
+  return _db_output
 
-    m = re.match(u'.*装(.*)。$', line)
-    if m:
-      value = m.group(1)
-      continue
 
-    m = re.match(u'唠：“(.*?)”', line)
-    if m:
-      output += m.group(1) + '\n'
-      continue
-
-  if value is not None:
-    output += str(value)
-  return output
+if __name__ == '__main__':
+  with io.open(sys.argv[1], 'r', encoding='utf-8') as src_file:
+    Run(src_file.read())
--- test/dongbei_test.py
+++ test/dongbei_test.py
@@ -10,18 +10,20 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from src import dongbei
 
+Token = dongbei.Token
+
 class DongbeiTest(unittest.TestCase):
   def testRunEmptyProgram(self):
     self.assertEqual(dongbei.Run(''), '')
 
   def testRunHelloWorld(self):
     self.assertEqual(
-        dongbei.Run(u'唠：“这嘎哒嗷嗷美好哇！”'),
+        dongbei.Run(u'唠：“这嘎哒嗷嗷美好哇！”。'),
         u'这嘎哒嗷嗷美好哇！\n')
 
   def testRunHelloWorld2(self):
     self.assertEqual(
-        dongbei.Run(u'唠：“你那嘎哒也挺美好哇！”'),
+        dongbei.Run(u'唠：“你那嘎哒也挺美好哇！”。'),
         u'你那嘎哒也挺美好哇！\n')
 
   def testVarDecl(self):
@@ -30,7 +32,61 @@ class DongbeiTest(unittest.TestCase):
 
   def testVarAssignment(self):
     self.assertEqual(
-        dongbei.Run(u'老张是活雷锋。\n老张装250。'), '250')
+        dongbei.Run(u'老张是活雷锋。\n老张装250。\n唠：老张。'), '250\n')
+
+  def testTokenize(self):
+    self.assertEqual(
+        list(dongbei.BasicTokenize(u'老张')),
+        [Token(dongbei.TK_CHAR, u'老'),
+         Token(dongbei.TK_CHAR, u'张'),])
+    self.assertEqual(
+        list(dongbei.Tokenize(u'# 123456\n老张')),
+        [Token(dongbei.TK_IDENTIFIER, u'老张')])
+    self.assertEqual(
+        list(dongbei.Tokenize(u'老张')),
+        [Token(dongbei.TK_IDENTIFIER, u'老张')])
+    self.assertEqual(
+        dongbei.ParseInteger(u'老张'),
+        (None, u'老张'))
+    self.assertEqual(
+        list(dongbei.ParseChars(u'老张')),
+        [Token(dongbei.TK_IDENTIFIER, u'老张')])
+    self.assertEqual(
+        list(dongbei.Tokenize(u'老张是活雷锋')),
+        [Token(dongbei.TK_IDENTIFIER, u'老张'),
+         Token(dongbei.TK_KEYWORD, u'是活雷锋')])
+    self.assertEqual(
+        list(dongbei.Tokenize(u'老张是活雷锋。\n老王是活雷锋。\n')),
+        [Token(dongbei.TK_IDENTIFIER, u'老张'),
+         Token(dongbei.TK_KEYWORD, u'是活雷锋'),
+         Token(dongbei.TK_KEYWORD, u'。'),
+         Token(dongbei.TK_IDENTIFIER, u'老王'),
+         Token(dongbei.TK_KEYWORD, u'是活雷锋'),
+         Token(dongbei.TK_KEYWORD, u'。'),
+        ])
+    self.assertEqual(
+        list(dongbei.Tokenize(u'老张装250。\n老王装老张。\n')),
+        [Token(dongbei.TK_IDENTIFIER, u'老张'),
+         Token(dongbei.TK_KEYWORD, u'装'),
+         Token(dongbei.TK_INTEGER_LITERAL, 250),
+         Token(dongbei.TK_KEYWORD, u'。'),
+         Token(dongbei.TK_IDENTIFIER, u'老王'),
+         Token(dongbei.TK_KEYWORD, u'装'),
+         Token(dongbei.TK_IDENTIFIER, u'老张'),
+         Token(dongbei.TK_KEYWORD, u'。')])
+    self.assertEqual(
+        list(dongbei.Tokenize(u'唠：“你好”。')),
+        [Token(dongbei.TK_KEYWORD, u'唠'),
+         Token(dongbei.TK_KEYWORD, u'：'),
+         Token(dongbei.TK_KEYWORD, u'“'),
+         Token(dongbei.TK_STRING_LITERAL, u'你好'),
+         Token(dongbei.TK_KEYWORD, u'”'),
+         Token(dongbei.TK_KEYWORD, u'。')])
+
+  def testVarAssignmentFromVar(self):
+    self.assertEqual(
+        dongbei.Run(u'老张是活雷锋。\n老王是活雷锋。\n'
+                    u'老张装250。\n老王装老张。\n唠：老王。'), '250\n')
 
 if __name__ == '__main__':
   unittest.main()
